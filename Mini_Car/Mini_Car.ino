d655dea151274f67038322b5509f212cd474cfca
tranhuubinh <tranhuubinh223@gmail.com>
2021-04-21 11:43:43 +0700

    add bluetooth controller

--- Mini_Car/Mini_Car.ino
+++ Mini_Car/Mini_Car.ino
@@ -1,8 +1,10 @@
+//#include <TimerOne.h>
 #include <IRremote.h>
-#define L1 11
-#define L2 10
-#define R1 9
-#define R2 8
+#define THRESHOLD 700
+#define L1 9
+#define L2 8
+#define R1 5
+#define R2 4
 #define IRPIN A0
 #define btUP 0x18
 #define btDOWN 0x52
@@ -12,29 +14,61 @@
 #define btNUM1 0x45
 #define btNUM2 0x46
 #define btNUM3 0x47
-#define THRESHOLD 600
+
 typedef enum {STOPC, FORWARD, BACKWARD, LEFT, RIGHT} CarState;
 typedef enum {STOPW, CW, CCW} WheelState;
 typedef enum {L, R} Wheel;
 byte speed = 120;
-unsigned long runtime;
-bool running = 0;
+unsigned long runtime = 0;
+bool running = false;
+byte mode = 1;
+void Mode();
 void controlCar(CarState s);
 void controlWheel(Wheel w, WheelState s );
 void IRControl();
+void LDRControl();
+void BluetoothControl();
 
 void setup() {
   pinMode(L2, OUTPUT);
   pinMode(R2, OUTPUT);
   controlCar(STOPC);
   Serial.begin(9600);
-  Serial.print(CW);
   IrReceiver.begin(IRPIN);
+  //  Timer1.initialize(1000);
+  //  Timer1.attachInterrupt(Mode);
 }
 
 void loop() {
+  //  switch (mode) {
+  //    case 1:
+  //      IRControl();
+  //      break;
+  //    case 2:
+  //      LDRControl();
+  //      break;
+  //    case 3:
+  //      BluetoothControl();
+  //  }
   IRControl();
-  LDRControl();
+  BluetoothControl();
+}
+
+void Mode() {
+  if (IrReceiver.decode()) {
+    Serial.println(IrReceiver.decodedIRData.command, HEX);
+    switch (IrReceiver.decodedIRData.command) {
+      case btNUM1:
+        mode = 1;
+        break;
+      case btNUM2:
+        mode = 2;
+        break;
+      case btNUM3:
+        mode = 3;
+    }
+    IrReceiver.resume();
+  }
 }
 
 void controlCar(CarState s) {
@@ -97,7 +131,7 @@ void controlWheel(Wheel w, WheelState s ) {
 
 void IRControl() {
   if (IrReceiver.decode()) {
-    Serial.println(IrReceiver.decodedIRData.command, HEX);
+    //Serial.println(IrReceiver.decodedIRData.command, HEX);
     switch (IrReceiver.decodedIRData.command) {
       case btUP:
         controlCar(FORWARD);
@@ -110,13 +144,12 @@ void IRControl() {
         break;
       case btRIGHT:
         controlCar(RIGHT);
-        break;
     }
     runtime = millis();
     running = true;
     IrReceiver.resume();
   }
-  if ( millis() - runtime > 300 && running) {
+  if ( millis() - runtime > 100 && running) {
     controlCar(STOPC);
     running = false;
   }
@@ -127,6 +160,7 @@ void LDRControl() {
   int down = analogRead(A2);
   int left = analogRead(A3);
   int right = analogRead(A4);
+  //Serial.println(String(up) + " " + String(down) + " " + String(left) + " " + String(right));
   if (up > THRESHOLD)
     controlCar(FORWARD);
   else if (down > THRESHOLD)
@@ -137,4 +171,25 @@ void LDRControl() {
     controlCar(RIGHT);
   else
     controlCar(STOPC);
+}
+
+void BluetoothControl() {
+  if (Serial.available()) {
+    switch (Serial.read()) {
+      case 'F':
+        controlCar(FORWARD);
+        break;
+      case 'B':
+        controlCar(BACKWARD);
+        break;
+      case 'L':
+        controlCar(LEFT);
+        break;
+      case 'R':
+        controlCar(RIGHT);
+        break;
+      case 'S':
+        controlCar(STOPC);
+    }
+  }
 }
