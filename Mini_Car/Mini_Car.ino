0f4c9af47ce90f5021b0a822d211eaf13cc456c7
tranhuubinh <tranhuubinh223@gmail.com>
2021-04-22 00:08:27 +0700

    add mode and rgb led

--- Mini_Car/Mini_Car.ino
+++ Mini_Car/Mini_Car.ino
@@ -1,6 +1,6 @@
-//#include <TimerOne.h>
+
 #include <IRremote.h>
-#define THRESHOLD 700
+#define THRESHOLD 600
 #define L1 9
 #define L2 8
 #define R1 5
@@ -10,24 +10,39 @@
 #define btDOWN 0x52
 #define btLEFT 0x8
 #define btRIGHT 0x5A
+#define RUNNING 0x0
 #define btOK 0x1C
 #define btNUM1 0x45
 #define btNUM2 0x46
 #define btNUM3 0x47
+#define btSpeedUp 0x16
+#define btSlowDown 0xD
+#define BLUEPIN 3
+#define GREENPIN 6
+#define REDPIN 10
+
+#define WHITE 0b000
+#define LIGHTBLUE  0b001
+#define PINK 0b010
+#define DARKBLUE 0b011
+#define LIGHTGREEN 0b100
+#define DARKGREEN 0b101
+#define RED 0b110
 
 typedef enum {STOPC, FORWARD, BACKWARD, LEFT, RIGHT} CarState;
 typedef enum {STOPW, CW, CCW} WheelState;
 typedef enum {L, R} Wheel;
-byte speed = 120;
+byte speed = 150;
 unsigned long runtime = 0;
 bool running = false;
 byte mode = 1;
-void Mode();
+
 void controlCar(CarState s);
 void controlWheel(Wheel w, WheelState s );
 void IRControl();
 void LDRControl();
 void BluetoothControl();
+void RGBLed(byte color);
 
 void setup() {
   pinMode(L2, OUTPUT);
@@ -35,40 +50,23 @@ void setup() {
   controlCar(STOPC);
   Serial.begin(9600);
   IrReceiver.begin(IRPIN);
-  //  Timer1.initialize(1000);
-  //  Timer1.attachInterrupt(Mode);
+  pinMode(BLUEPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(REDPIN, OUTPUT);
+
+  for (int i = 6; i >= 0; i--) {
+    RGBLed(i);
+    delay(250);
+  }
+  RGBLed(PINK);
 }
 
 void loop() {
-  //  switch (mode) {
-  //    case 1:
-  //      IRControl();
-  //      break;
-  //    case 2:
-  //      LDRControl();
-  //      break;
-  //    case 3:
-  //      BluetoothControl();
-  //  }
   IRControl();
-  BluetoothControl();
-}
-
-void Mode() {
-  if (IrReceiver.decode()) {
-    Serial.println(IrReceiver.decodedIRData.command, HEX);
-    switch (IrReceiver.decodedIRData.command) {
-      case btNUM1:
-        mode = 1;
-        break;
-      case btNUM2:
-        mode = 2;
-        break;
-      case btNUM3:
-        mode = 3;
-    }
-    IrReceiver.resume();
-  }
+  if (mode == 2)
+    LDRControl();
+  if (mode == 3)
+    BluetoothControl();
 }
 
 void controlCar(CarState s) {
@@ -94,6 +92,7 @@ void controlCar(CarState s) {
       controlWheel(R, STOPW);
   }
 }
+
 void controlWheel(Wheel w, WheelState s ) {
   switch (s) {
     case STOPW:
@@ -131,25 +130,64 @@ void controlWheel(Wheel w, WheelState s ) {
 
 void IRControl() {
   if (IrReceiver.decode()) {
-    //Serial.println(IrReceiver.decodedIRData.command, HEX);
+    Serial.println(IrReceiver.decodedIRData.command, HEX);
     switch (IrReceiver.decodedIRData.command) {
+      case btNUM1:
+        if (mode == 1) break;
+        mode = 1;
+        RGBLed(PINK);
+        break;
+      case btNUM2:
+        if (mode == 2) break;
+        mode = 2;
+        RGBLed(DARKGREEN);
+        break;
+      case btNUM3:
+        if (mode == 3) break;
+        mode = 3;
+        RGBLed(LIGHTBLUE);
+        break;
+        
       case btUP:
+        if (mode != 1) break;
         controlCar(FORWARD);
+        runtime = millis();
+        running = true;
         break;
       case btDOWN:
+        if (mode != 1) break;
         controlCar(BACKWARD);
+        runtime = millis();
+        running = true;
         break;
       case btLEFT:
+        if (mode != 1) break;
         controlCar(LEFT);
+        runtime = millis();
+        running = true;
         break;
       case btRIGHT:
+        if (mode != 1) break;
         controlCar(RIGHT);
+        runtime = millis();
+        running = true;
+        break;
+      case RUNNING:
+        runtime = millis();
+        running = true;
+        break;
+        
+      case btSpeedUp:
+        if (speed >= 110)
+          speed -= 10;
+        break;
+      case btSlowDown:
+        if (speed <= 240)
+          speed += 10;
     }
-    runtime = millis();
-    running = true;
     IrReceiver.resume();
   }
-  if ( millis() - runtime > 100 && running) {
+  if ( millis() - runtime > 120 && running) {
     controlCar(STOPC);
     running = false;
   }
@@ -192,4 +230,10 @@ void BluetoothControl() {
         controlCar(STOPC);
     }
   }
+}
+
+void RGBLed(byte color) {
+  digitalWrite(BLUEPIN, (color >> 2));
+  digitalWrite(GREENPIN, (color >> 1) & 1);
+  digitalWrite(REDPIN, color & 1);
 }
